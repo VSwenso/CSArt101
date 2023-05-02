b51638534d79cbb43502076bb9570059398b1df8
VSwenso <vlswenso@ucsc.edu>
2023-05-01 20:19:57 -0700

    basically all of lab 6

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,54 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// Author: Tory Swenson
+// Date: April 2023
+
+// Define the possible moves
+// const moves = ["rock", "paper", "scissors"];
+
+// Get the Computers move Randomly 
+// function getComputerMove() {
+  // const randonIndex = Math.floor(Math.random() * moves.length);
+  // return moves[randomIndex];
+// }
+
+// Play a single round of the game  
+// function playRound(playerMove, computerMove) {
+  // if (playerMove === computerMove) {
+    // return "It's a tie!";
+  // } else if (
+    // (playerMove === "rock" && computerMove === "scissors") ||
+    // (playerMove === "paper" && computerMove === "rock") ||
+    // (playerMove === "scissors" && computerMove === "paper")
+  // ) {
+    //return "You win!";
+  // } else {
+    //return "You lose!"; 
+  //  }
+// }
+
+// Play the Game 
+// function playGame() {
+  // let playerScore = 0; 
+  // let computerScore = 0; 
+
+  // for (let i = 0; i < 5; i++) {
+    // const playerMove = prompt("Enter your move: rock, paper, or scissors");
+    // const computerMove = getComputerMove();
+
+    // const result = playRound(playerMove, computerMove);
+    // console.log(result);
+
+    // if (result === "You win!") { 
+      // playerScore++; 
+    // } else if (result === "You lose") {
+      // computerScore++; 
+    // }
+  //  } 
+
+    // console.log('Final score: PLayer ${playerScore}, Computer ${computerScore}');
+  // } 
+  
+// playGame();  
 
 // Constants
 
