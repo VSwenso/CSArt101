8e0dcb30c3cbfb6dd71b335dc87f613c699c98e7
VSwenso <vlswenso@ucsc.edu>
2023-05-24 19:42:30 -0700

    lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,38 @@
+// index.js - lab 13: Loops  
+// Author: Tory Swenson
+// Date: 5/24/23
+
+// Constants 
+
+//Functions
+
+//this is an example of a functiom 
+function myFunction(param1, param2) {
+  //some code here
+  //return results;
+}
+
+function main() {
+  console.log("Main Function started."); 
+  // the code which makes stuff happen
+  function FIzzBuzz(){
+    var oneLongString= ''; 
+    for(var i=0; i<200; i++) {
+      var num=i; 
+      oneLongString+=num; 
+      if (i %3==0) {
+        oneLongString+='Fizz'
+      }
+      if (i %5==0) {
+        oneLongString+='Buzz'
+      }
+      if (i %7==0) {
+        oneLongString+='Boom'
+    }
+    oneLongString+='<br>'
+  
+  }
+  $('#output').append('<p>'+oneLongString+'</p>'); 
+}}
+
+main();
