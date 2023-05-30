8b9253fe10131b7223c8f6c29694464ae3184c6b
VSwenso <vlswenso@ucsc.edu>
2023-05-29 21:40:18 -0700

    lab 14 contents html

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -13,28 +13,5 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main Function started."); 
-  // the code which makes stuff happen
-  function FizzBuzz(){
-    var oneLongString = ""; 
-    for(var i = 0; i < 200; i++) {
-      var num = i; 
-      oneLongString += num; 
-      if (i % 3 == 0) {
-        oneLongString += 'Fizz';
-      }
-      if (i % 5 == 0) {
-        oneLongString += 'Buzz';
-      }
-      if (i % 7 == 0) {
-        oneLongString+='Boom'; 
-      }
-      oneLongString += '<br>'; 
-    }
-    $('#output').append('<p>'+ oneLongString + '</p>'); 
-  }
-
-  FizzBuzz();
 }
-
 main();
