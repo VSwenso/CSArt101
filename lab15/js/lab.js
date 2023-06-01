6a56ae6a409df255fe698704571051bd9a94379f
VSwenso <vlswenso@ucsc.edu>
2023-06-01 00:34:07 -0700

    java

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -13,31 +13,32 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main Function started."); 
-  //the code the makes stuff happen
+  console.log("Main function started.");
+  // the code that makes everything happen
 
   $(document).ready(function() {
-    // Attach the click event handler 
-    $(document).on('click', '#myButton', function() {
-      var randomNumber = Math.floor(Math.random() * 1015) + 1; 
-      console.log('Clicked!'); 
+    // Attach the click event handler to a parent element using event delegation
+    $(document).on('click', '#activate', function() {
+      var randomNumber = Math.floor(Math.random() * 1015) + 1;
+      console.log('Clicked!');
       $.ajax({
-        url: "https://pokieapi.co/api/v2/pokemon/" + randomNumber, 
-        type: "GET", 
-        dataType: "json", 
+        
+        url: "https://pokeapi.co/api/v2/pokemon/"+randomNumber,
+        type: "GET",
+        dataType: "json",
         success: function(data) {
-          //Actions to perform when the API call is unsuccessful
-          console.log(data); 
-          // Process the recieved data
-          var name = data.name; 
-          var spriteURL = data.sprites.front_default; 
-          $('#output').append('<p>Name: ' + name + '</p>'); 
-          $('#output').append('<img src="' + spriteURL + '" alt="Pokemon Sprite">'); 
-        }, 
-        error: function(jqXHR, textStatus, errorThrown) {
-          // Actions when the API call Fails
-          console.log("Error:", textStatus, errorThrown); 
-          // Handle the error condition  
+          // Actions to perform when the API call is successful
+          console.log(data);
+          // Process the received data or perform other tasks
+          var name = data.name;
+        var spriteURL = data.sprites.front_default;
+        $('#output').append('<p>Name: ' + name + '</p>');
+        $('#output').append('<img src="' + spriteURL + '" alt="Pokemon Sprite">');
+        },
+        error: function(jqXHR, textStatus, errorThrown) { 
+          // Actions to perform when the API call fails
+          console.log("Error:", textStatus, errorThrown);
+          // Handle the error condition appropriately
         }
       });
     });
