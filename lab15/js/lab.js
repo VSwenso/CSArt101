ac74d608857fe984d07eaf0ebf63443f74f7e53a
VSwenso <vlswenso@ucsc.edu>
2023-05-31 23:13:58 -0700

    my button

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -17,8 +17,8 @@ function main() {
   //the code the makes stuff happen
 
   $(document).ready(function() {
-    // Attach the click event handler to a parent element usin event delegation
-    $(document).on('click', '#activate', function() {
+    // Attach the click event handler 
+    $(document).on('click', '#myButton', function() {
       var randomNumber = Math.floor(Math.random() * 1015) + 1; 
       console.log('Clicked!'); 
       $.ajax({
@@ -28,16 +28,16 @@ function main() {
         success: function(data) {
           //Actions to perform when the API call is unsuccessful
           console.log(data); 
-          // Process the recieved data or perform other tasks
+          // Process the recieved data
           var name = data.name; 
           var spriteURL = data.sprites.front_default; 
           $('#output').append('<p>Name: ' + name + '</p>'); 
           $('#output').append('<img src="' + spriteURL + '" alt="Pokemon Sprite">'); 
         }, 
         error: function(jqXHR, textStatus, errorThrown) {
-          // Actions to perform when the API call Fails
+          // Actions when the API call Fails
           console.log("Error:", textStatus, errorThrown); 
-          // Handle the error condition appropriately 
+          // Handle the error condition  
         }
       });
     });
