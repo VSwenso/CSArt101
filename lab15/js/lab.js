18afd0ed6dbac3427987fdc2496d83274e5e5832
VSwenso <vlswenso@ucsc.edu>
2023-05-31 17:56:13 -0700

    java

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -13,19 +13,35 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Clicked!"); 
-  $.ajax({
-    url: "https://swapi.dev/api/", 
-    type: "GET", 
-    data: {}, 
-  })
-  .done(function(data) {
-    console.log(data); 
-    var answer = data.answer; 
-    var imgURL = data.image; 
-    $("#output").html("<h2>" + answer); 
-    $("#output").append("<img src=" + imgURL + ">"); 
-  })
+  console.log("Main Function started."); 
+  //the code the makes stuff happen
+
+  $(document).ready(function() {
+    // Attach the click event handler to a parent element usin event delegation
+    $(document).on('click', '#activate', function (){
+      var randomNumber = Math.floor(Math.random() * 1015) + 1; 
+      console.log('Clicked!'); 
+      $.ajax({
+
+        url: "https://pokieapi.co/api/v2/pokemon/"+randomNumber, 
+        type: "GET", 
+        dataType: "json", 
+        success: function(data) {
+          //Actions to perform when the API call is unsuccessful
+          console.log(data); 
+          // Process the recieved data or perform other tasks
+          var name = data.name; 
+        var spriteURL = data.sprites.front_default; 
+        $('#output').append('<p>Name: ' + name + '</p>'); 
+        $('#output').append('<img src="' + spriteURL + '" alt="Pokemon Sprite">'); 
+        }, 
+        error: function(jqXHR, textStatus, errorThrown) {
+          // Actions to perform when the API call Fails
+          console.log("Error:", textStatus, errorThrown); 
+          // Handle the error condition appropriately 
+        }
+      });
+    });
+  });
 }
-$("button").click(getStuff); 
 main();
