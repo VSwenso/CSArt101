60f5af1d1813448371ceef883c838a2a0c2e7518
VSwenso <vlswenso@ucsc.edu>
2023-06-05 22:55:46 -0700

    java

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -14,13 +14,14 @@ function myFunction(param1, param2) {
 
 $(document).ready(function() {
 
-  const api_url = 'https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0'
-  async function getInsight() {
+  const api_url ='https://api.nasa.gov/insight_weather/7api_key=DEMO_KEY&feedtype=json&ver=l.0'
+  async function getInsight(){
 
     const response= await fetch(api_url); 
     const data = await response.json(); 
     console.log(data); 
-
+    var sol = data.sol_keys[0]; 
+    $('#output').append('<p>'+data.sol_keys+'<p>'); 
   }
   getInsight(); 
-}); 
+})
